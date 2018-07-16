1893c43dd9492165b8b101d280f0f818fd277cbe
Alex Puhl <apuhlswimmer@hotmail.com>
2018-07-16 15:59:18 -0600

    added check for undefined

--- day1_set1.js
+++ day1_set1.js
@@ -1,23 +1,29 @@
 'use strict';
+
 function yearOfBirth(age) {
-  let yearOfBirth = 2018 - age;
-  return yearOfBirth;
+    if (age < 0) {
+        throw new Error('Age cannot be negative');
+    }
+    let yearOfBirth = 2018 - age;
+    return yearOfBirth;
 }
 
 
 function whoAmI(name, age) {
-    if (age<0) {
-        throw new Error ('Age cannot be negative');
-    }
+  if(typeof name !== "string" || typeof age !== 'number') {
+    console.error("Arguments not valid");
+    return;
+  }
+  try {
     let yob = yearOfBirth(age);
     console.log(`Hi, my is ${name} and I'm ${age} years old`);
     console.log(`I was born in ${yob}`);
+  } catch (e) {
+    console.error(e.message);
+  }
 }
 
-
-try {
-    whoAmI("alex", 32);
-    whoAmI('John', -5);
-   } catch(e) {
-    console.error(e.message);
-   }
+whoAmI("alex", 28);
+whoAmI('John', -5);
+whoAmI('Iolanthe', -8);
+whoAmI();
