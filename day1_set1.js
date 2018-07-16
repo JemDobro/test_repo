88f579b51f5c1a20e376d626db9d10b538e7d536
Jemadean Dobrosielski <jemadean@gmail.com>
2018-07-16 14:04:38 -0700

    added try catch

--- day1_set1.js
+++ day1_set1.js
@@ -4,9 +4,20 @@ function yearOfBirth(age) {
   return yearOfBirth;
 }
 
+
 function whoAmI(name, age) {
+    if (age<0) {
+        throw new Error ('Age cannot be negative');
+    }
     let yob = yearOfBirth(age);
     console.log(`Hi, my is ${name} and I'm ${age} years old`);
     console.log(`I was born in ${yob}`);
 }
-whoAmI("alex", 32);
+
+
+try {
+    whoAmI("alex", 32);
+    whoAmI('John', -5);
+   } catch(e) {
+    console.error(e.message);
+   }
