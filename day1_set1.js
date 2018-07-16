d2ac94902e095bd78cf21135a3e34255fe27b4d3
Alex Puhl <apuhlswimmer@hotmail.com>
2018-07-16 14:12:44 -0600

    broke code

--- day1_set1.js
+++ day1_set1.js
@@ -1,7 +1,11 @@
 'use strict';
+function yearOfBirth(age) {
+  let yearOfBirth = 2018 - age;
+}
+
 function whoAmI(name, age) {
-    let yearOfBirth = 2018 - age;
     console.log(`Hi, my is ${name} and I'm ${age} years old`);
     console.log(`I was born in ${yearOfBirth}`);
 }
 whoAmI("alex", 32);
+yearOfBirth(40);
