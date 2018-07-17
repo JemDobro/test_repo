77427056694f0ad3ab720a2f28e3f71383fd31b9
Alex Puhl <apuhlswimmer@hotmail.com>
2018-07-16 18:48:07 -0600

    finished To Infinity

--- day1_set2.js
+++ day1_set2.js
@@ -2,4 +2,24 @@
 function jediName(firstName, lastName) {
     return `${lastName.slice(0,3)}${firstName.slice(0,2)}`;
 }
-console.log(jediName('Beyonce', 'Knowles'));
+// console.log(jediName('Beyonce', 'Knowles'));
+
+
+
+function beyond(num) {
+  if (num === Infinity || num === -Infinity) {
+    console.log("And beyond")
+  } else if(num === 0) {
+    console.log("Staying home");
+  } else if(num < 0) {
+    console.log("To negative infinity");
+  } else if (num > 0) {
+    console.log("To infinity");
+  }
+}
+
+beyond(0);
+beyond(-5);
+beyond(5);
+beyond(Infinity);
+beyond(-Infinity);
